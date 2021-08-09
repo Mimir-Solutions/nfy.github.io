dc32a8e76a8f0ef945eb0471b80e20cdc4aee4c8
unknown <john_riley2@student.uml.edu>
2021-08-09 10:58:21 -0400

    Pages WIP
    Trying to get the app uploaded to GitPages and it only shows the readme for Yarn commands.

--- src/views/BitAlliance.js
+++ src/views/BitAlliance.js
@@ -1,17 +1,20 @@
 import React from 'react';
-import { Typography } from '@material-ui/core';
+import Main2 from '../components/Main2';
 import "../../src/index.css"
-// import Main2 from '../components/Main2';
+import {Typography} from '@material-ui/core';
+
+
 
 
 function BitAlliance() {
-    return <div>
-     {/* <div className= "bitAlliance-view" id="bitAlliance-view">
-    <Main2 currency="BitAlliance"/>
-    </div> */}
-    );
-   
-    <Typography variant="h5"style={{textAlign:'center', fontWeight:'500px', fontFamily:'Verdana', color:'#0000f' }}>Bit Alliance Coming Soon</Typography>
+    
+    return (  
+    <div 
+    // className= "bitAlliance-view" id="bitAlliance-view"
+    >
+    {/* <Main2 currency="BitAlliance"/> */}
+    
+    <Typography variant="h5"style={{textAlign:'center', fontWeight:'500px', fontFamily:'Verdana', color:'#0000f' }}>Bit Alliance Coming Soon!</Typography>
     <br></br>
     
     <br></br>
@@ -19,11 +22,17 @@ function BitAlliance() {
     <Typography variant="h6"style={{textAlign:'center', marginLeft:'100px',marginRight:'100px', color:'#39FF14' }}>“Tracking your investments is what we can give you, more tokens coming soon”
 </Typography>
 <br></br>
-
-
     </div>
-    
+
+
+
+
+    );
     
 }
+   
+   
+    
+    
 
 export default BitAlliance;
