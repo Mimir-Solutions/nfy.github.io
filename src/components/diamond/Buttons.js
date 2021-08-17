2fc90e90f16c2b1d580aa848fdd193aa2db7a128
Arif Sanaullah <ak7896023@gmail.com>
2021-08-17 22:11:56 +0500

    feat: Add page 1

---
+++ src/components/diamond/Buttons.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import { Button, Grid } from '@material-ui/core';
+import { makeStyles } from '@material-ui/styles';
+import clsx from 'clsx';
+
+const useStyles = makeStyles((theme) => ({
+    container: {
+        marginTop: theme.spacing(3),
+    },
+    btn: {
+        display: 'flex',
+        alignItems: 'center',
+    },
+    backBtn: {
+        justifyContent: 'flex-start',
+    },
+    nexBtn: {
+        justifyContent: 'flex-end',
+    },
+}));
+
+function Buttons({ step, setStep, allSteps }) {
+    const classes = useStyles();
+    return (
+        <Grid item xs={12} container className={classes.container}>
+            <Grid xs={6} className={clsx(classes.btn, classes.backBtn)}>
+                {step > 0 && (
+                    <Button
+                        onClick={() => {
+                            setStep(step - 1);
+                        }}
+                        variant="contained"
+                    >
+                        Previous
+                    </Button>
+                )}
+            </Grid>
+            <Grid item xs={6} className={clsx(classes.btn, classes.nexBtn)}>
+                {step < allSteps.length - 1 && (
+                    <Button
+                        onClick={() => {
+                            if (step < allSteps.length - 1) {
+                                setStep(step + 1);
+                            }
+                        }}
+                        variant="contained"
+                    >
+                        Next
+                    </Button>
+                )}
+            </Grid>
+        </Grid>
+    );
+}
+
+export default Buttons;
--- src/views/Diamond.js
+++ src/views/Diamond.js
@@ -1,14 +1,37 @@
-import React from 'react';
-// import { Typography } from '@material-ui/core';
-import "../../src/index.css"
+import React, { useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+
+import Buttons from '../components/diamond/Buttons';
+import Page1 from '../components/diamond/Page1';
+import Page2 from '../components/diamond/Page2';
+import Page3 from '../components/diamond/Page3';
+import { Grid } from '@material-ui/core';
+
+const useStyles = makeStyles((theme) => ({
+    container: {},
+}));
 
 function Diamond() {
-    return <div>
-   
-   
-    </div>
-    
-    
+    const styles = useStyles();
+
+    const [step, setStep] = useState(0);
+    const [selectedBlocks, setSelectedBlocks] = useState([]);
+
+    const allSteps = [
+        <Page1
+            selectedBlocks={selectedBlocks}
+            setSelectedBlocks={setSelectedBlocks}
+        />,
+        <Page2 />,
+        <Page3 />,
+    ];
+
+    return (
+        <Grid container className={styles.container}>
+            {allSteps[step]}
+            <Buttons allSteps={allSteps} step={step} setStep={setStep} />
+        </Grid>
+    );
 }
 
 export default Diamond;
